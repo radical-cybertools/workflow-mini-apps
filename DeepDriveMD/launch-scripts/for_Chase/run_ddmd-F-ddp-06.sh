f9de3fc8d25c9e72cbe25a9d8d92b857d30e3539
Tianle Wang <twang3@polaris-login-02.hsn.cm.polaris.alcf.anl.gov>
2024-09-27 16:27:32 +0000

    Add example script for Chase to run data collection

---
+++ DeepDriveMD/launch-scripts/for_Chase/run_ddmd-F-ddp-06.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+
+exp_dir=/eagle/RECUP/twang/miniapp-exp/ddmd-reprod-v2
+
+if [ -d ${exp_dir} ]
+then
+	echo "Error! Directory ${exp_dir} exists"
+	exit -1
+fi
+
+mkdir -p ${exp_dir}/model
+mkdir -p ${exp_dir}/data
+
+num_phase=3
+for((i=0; i<num_phase; i++))
+do
+	mkdir -p ${exp_dir}/data/phase${i}
+done
+
+
+python ../rct-scripts/ddmd-F-ddp.py	\
+	--num_phases        ${num_phase}	\
+	--mat_size 		20000		\
+	--data_root_dir		"${exp_dir}/data"	\
+	--num_step		25000		\
+	--num_epochs_train	125		\
+	--model_dir		"${exp_dir}/model"	\
+	--conda_env		        "/eagle/RECUP/twang/env/rose-task-base-clone"	\
+	--num_sample		1000		\
+	--num_mult_train	4000		\
+	--dense_dim_in		12544		\
+	--dense_dim_out		192		\
+	--preprocess_time_train	30		\
+	--preprocess_time_agent	5		\
+	--num_epochs_agent	125		\
+	--num_mult_agent	2000		\
+	--num_mult_outlier	200		\
+    --allreduce_size    6422528 \
+	--project_id		RECUP		\
+	--queue			"debug"		\
+	--num_sim		8		\
+	--num_nodes		2		\
+	--io_json_file		"io_size.json" 
